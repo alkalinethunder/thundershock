976bfe9fac54149e3665c98b39bb8486d782321b
Michael VanOverbeek <theplexgate@gmail.com>
2021-06-14 07:36:56 -0400

    Add TextureComponent scene component.

---
+++ src/Thundershock/Components/TextureComponent.cs
@@ -0,0 +1,36 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Thundershock.Rendering;
+
+namespace Thundershock.Components
+{
+    public class TextureComponent : SceneComponent
+    {
+        public Color Color { get; set; } = Color.White;
+        public Vector2 Position { get; set; }
+        public Vector2 Origin { get; set; } = new Vector2(0.5f, 0.5f);
+        public Vector2 Pivot { get; set; } = new Vector2(0.5f, 0.5f);
+        public Vector2 Size { get; set; } = new Vector2(50, 50);
+        public Texture2D Texture { get; set; }
+        
+        protected override void OnDraw(GameTime gameTime, Renderer batch)
+        {
+            var rect = batch.ViewportBounds;
+
+            var origin = rect.Location.ToVector2() + (rect.Size.ToVector2() * Origin);
+
+            var pivot = Size * Pivot;
+
+            var pos = origin - pivot + Position;
+
+            rect.X = (int) pos.X;
+            rect.Y = (int) pos.Y;
+            rect.Width = (int) Size.X;
+            rect.Height = (int) Size.Y;
+
+            batch.Begin();
+            batch.FillRectangle(rect, Color, Texture);
+            batch.End();
+        }
+    }
+}
