4f8c0a689fec11426cd2d8b398589774b9eab23c
Alkaline Thunder <theplexgate@gmail.com>
2021-08-15 10:38:43 -0400

    Code cleanup, somewhat

--- src/Thundershock.Core/AppExitEventArgs.cs
+++ src/Thundershock.Core/AppExitEventArgs.cs
@@ -2,10 +2,19 @@ using System;
 
 namespace Thundershock.Core
 {
+    /// <summary>
+    /// Contains the event parameters for a Thundershock application exit event.
+    /// </summary>
     public sealed class AppExitEventArgs : EventArgs
     {
+        /// <summary>
+        /// Gets a value indicating whether the exit operation has been cancelled.
+        /// </summary>
         public bool CancelExit { get; private set; }
-
+        
+        /// <summary>
+        /// Cancels the exit operation.
+        /// </summary>
         public void Cancel()
         {
             CancelExit = true;
